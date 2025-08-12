0304914b02918d460e0f61e2ac70813daffe080e
PerinpanayagamGajan <perinpanayagamgajan0@gmail.com>
2025-08-12 10:36:58 +0530

    Don histing -trial 2 with port changes

--- Backend/index.js
+++ Backend/index.js
@@ -2609,6 +2609,7 @@ app.get('/getproductsbill', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on por ${PORT}`);
 });
